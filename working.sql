c990443063878196c3c0bebe399aa348a312ab78
M.T.Tharindi <tharindi.20240581@iit.ac.lk>
2026-01-03 05:39:43 +0000

    Update SQL query to select shirt_or_hat and state_code along with first and last names, allowing for multiple state codes

--- working.sql
+++ working.sql
@@ -1,3 +1,3 @@
-SELECT first_name, last_name
+SELECT shirt_or_hat, state_code, first_name, last_name
 FROM people
-WHERE state_code='CA' AND shirt_or_hat='shirt' AND team='Angry Ants';
+WHERE (state_code='CA' OR state_code='CO') AND shirt_or_hat='shirt' AND team='Angry Ants';
