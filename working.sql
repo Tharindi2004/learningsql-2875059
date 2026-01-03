8a34e58a358a03e3630e033146ec9b33169fd8da
M.T.Tharindi <tharindi.20240581@iit.ac.lk>
2026-01-03 05:44:20 +0000

    Refactor SQL query to select first and last names from the people table where first_name matches the pattern 'B%N'

--- working.sql
+++ working.sql
@@ -1,3 +1,2 @@
-SELECT shirt_or_hat, state_code, first_name, last_name
-FROM people
-WHERE (state_code='CA' OR state_code='CO') AND shirt_or_hat='shirt' AND team='Angry Ants';
+SELECT first_name, last_name FROM people
+WHERE first_name LIKE 'B%N';
