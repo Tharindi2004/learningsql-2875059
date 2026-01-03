9ba81818d67ca0d39dbe2225800fa163375d57b6
M.T.Tharindi <tharindi.20240581@iit.ac.lk>
2026-01-03 08:56:50 +0000

    Update SQL query to filter results by first name and region

--- working.sql
+++ working.sql
@@ -1,5 +1,6 @@
 SELECT people.first_name, people.state_code, states. division
 FROM people
-JOIN states ON people.state_code = state_abbrev;
+JOIN states ON people.state_code = state_abbrev
+WHERE people.first_name LIKE 'J%' AND states.region = 'South';
 
 
