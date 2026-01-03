a653c0e2e7968228349663e7b64e0ea3877da1cd
M.T.Tharindi <tharindi.20240581@iit.ac.lk>
2026-01-03 14:31:07 +0000

    Replace INSERT statement with SELECT query for specific person and update last name in people table

--- working.sql
+++ working.sql
@@ -1,6 +1,3 @@
-INSERT INTO people
-(first_name,last_name)
-VALUES
-('George','Washington'),
-('John','Adams'),
-('Thomas','Jefferson');
+UPDATE people
+SET last_name = ''
+WHERE first_name = 'Calos' AND city = 'Houston';
